fbfd455b9c96dfd6dd68f2cf1e4cd6fb087b1ce8
Piotr Czajkowski <pczajkowski@gmail.com>
2021-03-22 10:59:25 +0100

    More tests

---
+++ encodedText_test.go
@@ -0,0 +1,20 @@
+package weirdtext
+
+import (
+	"testing"
+)
+
+func TestInvalidPrefix(t *testing.T) {
+	input := `
+---weir---
+Tihs is a lnog lnooog tset setcnnee,
+wtih smoe big (biiiiig) wdros!
+---weir---
+This long looong sentence some test with words`
+
+	test := EncodedText{}
+	err := test.FromString(input)
+	if err == nil {
+		t.Errorf("There should be error as prefix is invalid!")
+	}
+}
