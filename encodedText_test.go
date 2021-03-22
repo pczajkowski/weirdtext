88deb1abe9e3243ee3d006d50b693be0a42fa014
Piotr Czajkowski <pczajkowski@gmail.com>
2021-03-22 11:02:46 +0100

    Added TestInvalidString

--- encodedText_test.go
+++ encodedText_test.go
@@ -18,3 +18,17 @@ This long looong sentence some test with words`
 		t.Errorf("There should be error as prefix is invalid!")
 	}
 }
+
+func TestInvalidString(t *testing.T) {
+	input := `
+---weird---
+Tihs is a lnog lnooog tset setcnnee,
+wtih smoe big (biiiiig) wdros!
+---weird---`
+	test := EncodedText{}
+	err := test.FromString(input)
+	if err == nil {
+		t.Errorf("There should be error as string is invalid!")
+	}
+
+}
